7d2404c4c95173bfa192a0ebdc28ef12e7afd912
1024-1024 <zhangwl.freedom@gmail.com>
2016-09-14 10:12:36 +0800

    aa

---
+++ test/src/test1/Test3.java
@@ -0,0 +1,20 @@
+package test1;
+
+public class Test3 {
+	public static void main(String[] args) {
+		
+		int a = 0;
+		switch (a) {
+		case 0:
+			System.out.println("--0-");
+			break;
+		case 1: {
+			System.out.println("--1-");
+			break;	
+		}
+			
+		default:
+			break;
+		}
+	}
+}
